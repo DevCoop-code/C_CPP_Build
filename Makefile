c649b5b51beca995fce64d41d9540f09b6367d3e
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-07-19 23:43:42 +0900

    refactor, feat: add cpp study source code and make build system

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	$(MAKE) -C C_Cplusplus_BuildStorage build-all
-
-clean:
-	$(MAKE) -C C_Cplusplus_BuildStorage clean
--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
+rootBuildPath=$1
+export objectFileSet
+export sourceFileExtension
 
-echo
-echo "=====Build C/C++====="
+echo "=====Build C/C++ using gcc compiler====="
+
+# Delete all of old output data
+$(rm -rf output/*)
+
+if [ -f temp ]; then
+	echo "temp directory already exists"
+	
+	$(rm -rf temp/)
+	$(mkdir temp)	
+else
+	$(mkdir temp)
+fi
+
+# Compile the files
+sourceFileSet=$(ls ${rootBuildPath}/sources/*.cpp)
+
+for sourceFile in ${sourceFileSet[@]}; do
+	objectFileName=$(basename ${sourceFile})
+	objectNameWithoutExtension="${objectFileName%.*}"
+	sourceFileExtension="${objectFileName##*.}"
+
+	if [ ${sourceFileExtension} == "cpp" ]; then
+		g++ -c ${sourceFile} -I ${rootBuildPath}/headers/
+	else
+		gcc -c ${sourceFile} -I ${rootBuildPath}/headers/
+	fi
+	mv ${objectNameWithoutExtension}.o temp/
+
+	objectFileSet=${objectFileSet}temp/${objectNameWithoutExtension}.o
+done
+
+if [ ${sourceFileExtension} == 'cpp' ]; then
+	g++ $objectFileSet -o output/main
+else
+	gcc $objectFileSet -o output/main
+fi
+
+$(rm -rf temp/)
+
+./output/main
+
